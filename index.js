f776506d261f2501be0979d75da254b8cf55342f
LuckieCrab <luckie@luckiecrab.nl>
2025-05-07 22:41:29 +0200

    discord verify me pls

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/21_cars_meme.jpg', (req, res) => {
     res.sendFile(__dirname + '/views/21_cars_meme.jpg')
 })
 
+app.get('.well-known/discord', (req, res) => {
+    res.send('dh=83816783f4f2c312ebedc3ae4263f9edbae591e2')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
