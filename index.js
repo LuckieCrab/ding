a7ccad43dcf202d7869c48933196d7f6d4523352
LuckieCrab <luckie@luckiecrab.nl>
2025-05-07 22:25:55 +0200

    add img for top seo!!!

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
 
+app.get('/21_cars_meme.jpg', (req, res) => {
+    res.sendFile(__dirname + '/views/21_cars_meme.jpg')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
